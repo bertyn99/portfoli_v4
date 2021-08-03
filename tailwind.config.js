c80e39f8ec19826c472ee4761c6702f2ccbfbd91
bertyn99 <bboulikou@gmail.com>
2021-08-03 16:21:18 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif"
+      },
+      colors: {
+        primary: {
+          DEFAULT: 'hsl(250,69%,61%)',
+          'lighter': 'hsl(250,92%,85%)',
+          'alt': 'hsl(250,57%,53%)',
+          'title': 'hsl(250,8%,15%)',
+          'text': 'hsl(250,8%,61%)',
+          'textLight': 'hsl(250,8%,61%)',
+          'input': 'hsl(250,70%,96%)',
+          'body': 'hsl(250,60%,99%)',
+        },
+      }
+    }
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
