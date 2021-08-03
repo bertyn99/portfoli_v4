666e1a61404f52e0150d7463091d18a92c3c83ac
bertyn99 <bboulikou@gmail.com>
2021-08-03 20:29:34 +0200

    ajout css de base

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,5 +55,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      plugins: {
+        'postcss-nested': {},
+      }
+    }
   }
 }
