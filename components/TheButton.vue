83387c28a7239311d8711a6efa4eb5b1a4a469db
bertyn99 <bboulikou@gmail.com>
2021-08-04 23:09:51 +0200

    ajout du composant button

---
+++ components/TheButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <nuxt-link
+    :to="link"
+    class="
+      inline-flex
+      justify-between
+      bg-primary
+      text-white
+      p-4
+      rounded-lg
+      hover:bg-primary-alt
+    "
+    ><slot></slot>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    link: { type: String, require: true, default: '' },
+  },
+}
+</script>
+
+<style>
+</style>
