615d01e92b59b8d4de5a0a366585f56800b07e84
bertyn99 <bboulikou@gmail.com>
2021-08-07 20:15:29 +0200

    ajout du footer et du layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <Header />
-    <main class="main">
-      <home></home>
-      <about></about>
-      <skills></skills>
-      <graduate></graduate>
-      <services></services>
-      <portfolio></portfolio>
-    </main>
-  </div>
+  <main class="main">
+    <home></home>
+    <about></about>
+    <skills></skills>
+    <graduate></graduate>
+    <services></services>
+    <portfolio></portfolio>
+    <contactme></contactme>
+  </main>
 </template>
 
 <script>
@@ -19,7 +17,18 @@ import Skills from '~/components/Skills.vue'
 import Graduate from '~/components/Graduate.vue'
 import Portfolio from '~/components/Portfolio.vue'
 import Services from '~/components/Services.vue'
+import Contactme from '~/components/Contactme.vue'
+
 export default {
-  components: { Home, About, Portfolio, Skills, Graduate, Services },
+  components: {
+    Home,
+    About,
+    Portfolio,
+    Skills,
+    Graduate,
+    Services,
+    Contactme,
+  },
+  layout: 'default',
 }
 </script>
