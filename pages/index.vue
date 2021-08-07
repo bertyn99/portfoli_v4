2be21ae286414cf22024e639d71cb3fb4c727015
bertyn99 <bboulikou@gmail.com>
2021-08-07 15:42:38 +0200

    ajout des differentes section

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <Header />
-    <main class="main"></main>
+    <main class="main">
+      <home></home>
+      <about></about>
+      <skills></skills>
+      <graduate></graduate>
+      <services></services>
+      <portfolio></portfolio>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import Home from '~/components/Home.vue'
+import About from '~/components/About.vue'
+import Skills from '~/components/Skills.vue'
+import Graduate from '~/components/Graduate.vue'
+import Portfolio from '~/components/Portfolio.vue'
+import Services from '~/components/Services.vue'
+export default {
+  components: { Home, About, Portfolio, Skills, Graduate, Services },
+}
 </script>
