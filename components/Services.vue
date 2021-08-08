860a6175927f041b6d5c9f56d348ff8b44397140
bertyn99 <bboulikou@gmail.com>
2021-08-08 17:59:47 +0200

    aajout des service

--- components/Services.vue
+++ components/Services.vue
@@ -1,8 +1,160 @@
-<template><section></section></template>
+<template>
+  <section id="services" class="services section">
+    <h2 class="section-title">Services</h2>
+    <span class="section-subtitle">Que puis-je offrir?</span>
+    <div class="services-container container grid mx-auto px-5">
+      <div v-for="(service, i) in services" :key="i" class="services-content">
+        <div>
+          <svg
+            v-if="service.name == 'Ux/Ui'"
+            class="h-6 w-6 text-primary mb-4"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z"
+            />
+          </svg>
+          <svg
+            v-if="service.name == 'Website'"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-primary mb-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+            />
+          </svg>
+          <svg
+            v-if="service.name == 'Server'"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-primary mb-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01"
+            />
+          </svg>
+          <h3 class="services-title">
+            {{ service.name }} <br />
+            {{ service.post }}
+          </h3>
+        </div>
+
+        <!--    <span
+          class="button button-flex button-small button-link services-button"
+        ></span> -->
+        <div
+          class="services-button button-small button-flex button-link"
+          @click="service.isOpen = !service.isOpen"
+        >
+          Voir plus
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="button-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 7l5 5m0 0l-5 5m5-5H6"
+            />
+          </svg>
+        </div>
+        <services-modal
+          v-if="service.isOpen"
+          :class="[service.isOpen ? 'active-modal' : '']"
+          :service="service"
+          @close-modal="close(i)"
+        ></services-modal>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
-export default {}
+import ServicesModal from './ServicesModal.vue'
+
+export default {
+  components: { ServicesModal },
+  data() {
+    return {
+      services: [
+        {
+          name: 'Ux/Ui',
+          post: 'Designer',
+          isOpen: false,
+          skills: [
+            'Je developpe des interface utilisateur',
+            'Developpement de page web',
+            'Je crée des intéraction avec des élement Ux',
+          ],
+        },
+        {
+          name: 'Website',
+          post: 'Front-End',
+          isOpen: false,
+          skills: [
+            'Je developpe des interface utilisateur',
+            'Developpement de page web',
+          ],
+        },
+        {
+          name: 'Server',
+          post: 'Back-End',
+          isOpen: false,
+          skills: [
+            'Je developpe des serveur Api Rest',
+            "Je developpe des systeme d'administration",
+            "Je crée des systeme d'authentification",
+          ],
+        },
+      ],
+    }
+  },
+  methods: {
+    close(v) {
+      this.services[v].isOpen = false
+    },
+  },
+}
 </script>
 
-<style>
+<style lang="postcss">
+.services {
+  &-container {
+    @apply grid-cols-2 gap-6;
+  }
+  &-content {
+    @apply relative bg-primary-body pt-14 pr-2 pl-5 pb-6 rounded shadow-sm transition duration-300;
+    &:hover {
+      @apply shadow-md;
+    }
+  }
+  &-title {
+    @apply text-lg font-medium mb-4;
+  }
+  &-button {
+    @apply cursor-pointer text-sm;
+    &:hover > .button-icon {
+      @apply transform translate-x-2;
+    }
+  }
+}
+.active-modal {
+  opacity: 1;
+}
 </style>
---
+++ components/ServicesModal.vue
@@ -0,0 +1,91 @@
+<template>
+  <transition>
+    <div class="services-modal">
+      <div class="services-modal-content">
+        <h4 class="services-modal-title">
+          {{ service.name }} <br />
+          {{ service.post }}
+        </h4>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 services-modal-close"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          @click="closeModal"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+        <ul class="services-modal-services grid">
+          <li
+            v-for="(skill, i) in service.skills"
+            :key="i"
+            class="services-modal-service"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 services-modal-icon"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <p>{{ skill }}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    service: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close-modal')
+    },
+  },
+}
+</script>
+
+<style lang="postcss">
+.services-modal {
+  @apply fixed top-0 inset-x-0 h-screen bg-black bg-opacity-80 flex items-center justify-center px-0 py-4 z-30 opacity-0 transition duration-500;
+  &-content {
+    @apply relative bg-primary-body p-6 rounded-lg;
+  }
+
+  &-services {
+    @apply gap-4;
+  }
+  &-service {
+    @apply flex;
+  }
+  &-title {
+    @apply text-lg font-medium mb-6;
+  }
+  &-close {
+    @apply absolute top-4 right-4 text-2xl text-primary cursor-pointer;
+  }
+  &-icon {
+    @apply text-primary mr-1;
+  }
+}
+</style>
