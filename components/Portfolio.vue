266be2701cccfc3a37b1a3a4a418e30c02756645
bertyn99 <bboulikou@gmail.com>
2021-08-10 01:05:33 +0200

    ajout composant porfolio version mobile

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -1,10 +1,90 @@
 <template>
-  <section></section>
+  <section id="portfolio" class="portfolio section">
+    <h2 class="section-title">Portfolio</h2>
+    <span class="section-subtitle">Derniers Projet</span>
+    <div class="portfolio-container container mx-auto px-5">
+      <carousel>
+        <carousel-slide v-for="(p, i) in projects" :key="i" :index="i">
+          <div class="portfolio-content grid">
+            <img :src="`/img/${p.img}`" alt="" class="portfolio-img" />
+            <div class="portfolio-data">
+              <h3 class="portfolio-title">{{ p.name }}</h3>
+              <p class="portfolio-description">
+                {{ p.desc }}
+              </p>
+              <a
+                :href="p.link"
+                class="button button-flex button-small portfolio-button"
+                >Demo
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 button-icon"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 7l5 5m0 0l-5 5m5-5H6"
+                  />
+                </svg>
+              </a>
+            </div></div
+        ></carousel-slide>
+      </carousel>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import Carousel from '@/components/Carousel/carousel.vue'
+import CarouselSlide from '@/components/Carousel/carouselSlide.vue'
+export default {
+  components: { Carousel, CarouselSlide },
+  data() {
+    return {
+      projects: [
+        {
+          name: 'Site web Moderne',
+          img: 'hawa.png',
+          link: 'https://www.hawabowls.com/',
+          desc: 'Site web responsive avec des composants ui et des intéractions animées.',
+        },
+        {
+          name: 'Click n Chill',
+          link: 'https://clicknchill.net/',
+          img: 'v2.png',
+          desc: "Extension chrome qui permet de synchroniser le visionnage d'une série Netflix à plusieurs personnes.",
+        },
+      ],
+    }
+  },
+}
 </script>
 
-<style>
+<style lang="postcss">
+.portfolio {
+  &-contianer {
+    overflow: initial;
+  }
+  &-content {
+    @apply py-0 px-6;
+  }
+  &-img {
+    width: 256px;
+    height: 160px;
+    @apply rounded-lg justify-self-center;
+  }
+  &-title {
+    @apply text-lg mb-2;
+  }
+  &-description {
+    @apply mb-3;
+  }
+  &-button:hover > .button-icon {
+    @apply transform translate-x-2;
+  }
+}
 </style>
