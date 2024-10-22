45d2c65f56f019e301503ab9d8df4ea3fa937112
bertyn99 <bboulikou@gmail.com>
2024-10-22 16:25:23 +0200

    changer text

--- components/Home.vue
+++ components/Home.vue
@@ -1,81 +1,44 @@
-
 <template>
   <section class="home section">
-    <div
-      class="home-container container mx-auto px-4 sm:px-6 lg:px:8 grid gap-6"
-    >
+    <div class="home-container container mx-auto px-4 sm:px-6 lg:px:8 grid gap-6">
       <div class="home-content">
         <div class="home-social">
-          <a
-            href="https://www.facebook.com/helenbertyn.boulikoudamours"
-            class="home-social-icon"
-            ><svg
-              class="h-5 w-5"
-              enable-background="new 0 0 24 24"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+          <a href="https://www.facebook.com/helenbertyn.boulikoudamours" class="home-social-icon"><svg class="h-5 w-5"
+              enable-background="new 0 0 24 24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path
-                d="m6.812 13.937h2.518v9.312c0 .414.335.75.75.75l4.007.001c.415 0 .75-.337.75-.75v-9.312h2.387c.378 0 .697-.282.744-.657l.498-4c.056-.446-.293-.843-.744-.843h-2.885c.113-2.471-.435-3.202 1.172-3.202 1.088-.13 2.804.421 2.804-.75v-3.577c0-.375-.277-.692-.648-.743-.314-.043-1.555-.166-3.094-.166-7.01 0-5.567 7.772-5.74 8.437h-2.519c-.414 0-.75.336-.75.75v4c0 .414.336.75.75.75zm.75-3.999h2.518c.414 0 .75-.336.75-.75v-3.151c0-2.883 1.545-4.536 4.24-4.536.878 0 1.686.043 2.242.087v2.149c-.402.205-3.976-.884-3.976 2.697v2.755c0 .414.336.75.75.75h2.786l-.312 2.5h-2.474c-.414 0-.75.336-.75.75v9.311h-2.505v-9.312c0-.414-.336-.75-.75-.75h-2.519z"
-              /></svg
-          ></a>
-          <a href="https://github.com/bertyn99" class="home-social-icon"
-            ><svg class="h-5 w-5" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"
-              />
+                d="m6.812 13.937h2.518v9.312c0 .414.335.75.75.75l4.007.001c.415 0 .75-.337.75-.75v-9.312h2.387c.378 0 .697-.282.744-.657l.498-4c.056-.446-.293-.843-.744-.843h-2.885c.113-2.471-.435-3.202 1.172-3.202 1.088-.13 2.804.421 2.804-.75v-3.577c0-.375-.277-.692-.648-.743-.314-.043-1.555-.166-3.094-.166-7.01 0-5.567 7.772-5.74 8.437h-2.519c-.414 0-.75.336-.75.75v4c0 .414.336.75.75.75zm.75-3.999h2.518c.414 0 .75-.336.75-.75v-3.151c0-2.883 1.545-4.536 4.24-4.536.878 0 1.686.043 2.242.087v2.149c-.402.205-3.976-.884-3.976 2.697v2.755c0 .414.336.75.75.75h2.786l-.312 2.5h-2.474c-.414 0-.75.336-.75.75v9.311h-2.505v-9.312c0-.414-.336-.75-.75-.75h-2.519z" />
+            </svg></a>
+          <a href="https://github.com/bertyn99" class="home-social-icon"><svg class="h-5 w-5" viewBox="0 0 24 24">
+              <path fill="currentColor"
+                d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" />
             </svg>
           </a>
-          <a
-            href="https://www.linkedin.com/in/bertyn-boulikou/"
-            class="home-social-icon"
-            ><svg
-              id="Bold"
-              enable-background="new 0 0 24 24"
-              class="h-4 w-5"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+          <a href="https://www.linkedin.com/in/bertyn-boulikou/" class="home-social-icon"><svg id="Bold"
+              enable-background="new 0 0 24 24" class="h-4 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path
-                d="m23.994 24v-.001h.006v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07v-2.185h-4.773v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243v7.801z"
-              />
+                d="m23.994 24v-.001h.006v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07v-2.185h-4.773v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243v7.801z" />
               <path d="m.396 7.977h4.976v16.023h-4.976z" />
               <path
-                d="m2.882 0c-1.591 0-2.882 1.291-2.882 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909c-.001-1.591-1.292-2.882-2.882-2.882z"
-              /></svg
-          ></a>
+                d="m2.882 0c-1.591 0-2.882 1.291-2.882 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909c-.001-1.591-1.292-2.882-2.882-2.882z" />
+            </svg></a>
           <a href="" class="home-social-icon"></a>
         </div>
 
         <div class="home-img">
-          <svg
-            class="home-blob"
-            viewBox="0 0 200 187"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-          >
+          <svg class="home-blob" viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink">
             <mask id="mask0" mask-type="alpha">
-              <path
-                d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+              <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
         130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
         97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-              />
+        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z" />
             </mask>
             <g mask="url(#mask0)">
-              <path
-                d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
+              <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
         165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
         129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
-              />
-              <image
-                class="home-blob-img"
-                x="14"
-                y="18"
-                xlink:href="/img/bertyn.png"
-              />
+        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z" />
+              <image class="home-blob-img" x="14" y="18" xlink:href="/img/bertyn.png" />
             </g>
           </svg>
         </div>
@@ -83,54 +46,27 @@
           <h1 class="home-title">Salut, Je m'appelle Bertyn</h1>
           <h3 class="home-subtitle">Web developpeur</h3>
           <p class="home-description">
-            Étudiant en web développement, produisant du travail de qualité et
+            Freelance Developpeur fullstack, produisant du travail de qualité et
             toujours curieux de découvrir de nouvelles technologies.
           </p>
-          <the-button link="#contact" class="button button-flex"
-            >Contactez-moi<svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="button-icon"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              /></svg
-          ></the-button>
+          <the-button link="#contact" class="button button-flex">Contactez-moi<svg xmlns="http://www.w3.org/2000/svg"
+              class="button-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg></the-button>
         </div>
       </div>
       <div class="home-scroll">
         <nuxt-link to="#about" class="home-scroll-button button-flex">
-          <svg
-            class="h-8 w-6"
-            viewBox="-105 0 512 512.00002"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg class="h-8 w-6" viewBox="-105 0 512 512.00002" xmlns="http://www.w3.org/2000/svg">
             <path
-              d="m150.601562 0c-83.042968 0-150.601562 67.558594-150.601562 150.601562v210.796876c0 83.042968 67.558594 150.601562 150.601562 150.601562 83.042969 0 150.601563-67.558594 150.601563-150.601562v-210.796876c0-83.042968-67.558594-150.601562-150.601563-150.601562zm119.882813 361.398438c0 66.101562-53.78125 119.882812-119.882813 119.882812-66.101562 0-119.882812-53.78125-119.882812-119.882812v-210.796876c0-66.101562 53.78125-119.882812 119.882812-119.882812 66.101563 0 119.882813 53.78125 119.882813 119.882812zm0 0"
-            />
+              d="m150.601562 0c-83.042968 0-150.601562 67.558594-150.601562 150.601562v210.796876c0 83.042968 67.558594 150.601562 150.601562 150.601562 83.042969 0 150.601563-67.558594 150.601563-150.601562v-210.796876c0-83.042968-67.558594-150.601562-150.601563-150.601562zm119.882813 361.398438c0 66.101562-53.78125 119.882812-119.882813 119.882812-66.101562 0-119.882812-53.78125-119.882812-119.882812v-210.796876c0-66.101562 53.78125-119.882812 119.882812-119.882812 66.101563 0 119.882813 53.78125 119.882813 119.882812zm0 0" />
             <path
-              d="m150.605469 101.851562c-8.484375 0-15.359375 6.875-15.359375 15.359376v70.695312c0 8.480469 6.875 15.359375 15.359375 15.359375 8.480469 0 15.359375-6.878906 15.359375-15.359375v-70.695312c0-8.484376-6.875-15.359376-15.359375-15.359376zm0 0"
-            />
+              d="m150.605469 101.851562c-8.484375 0-15.359375 6.875-15.359375 15.359376v70.695312c0 8.480469 6.875 15.359375 15.359375 15.359375 8.480469 0 15.359375-6.878906 15.359375-15.359375v-70.695312c0-8.484376-6.875-15.359376-15.359375-15.359376zm0 0" />
           </svg>
           <span class="home-scroll-name">Scroll down</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-4 w-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17 13l-5 5m0 0l-5-5m5 5V6"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13l-5 5m0 0l-5-5m5 5V6" />
           </svg>
         </nuxt-link>
       </div>
@@ -151,16 +87,20 @@ export default {
     display: grid;
     grid-template-columns: 0.25fr 3fr;
     padding-top: 3.5rem;
+
     @screen sm {
       grid-template-columns: 0.5fr 3fr;
     }
+
     @screen md {
       grid-template-columns: max-content 1fr 1fr;
     }
+
     @screen lg {
       @apply pt-20;
     }
   }
+
   &-container {
     @screen lg {
       @apply gap-20;
@@ -171,65 +111,85 @@ export default {
     display: grid;
     grid-template-columns: max-content;
     row-gap: 1rem;
+
     @screen md {
       @apply mr-8;
     }
+
     @screen xl {
       @apply transform -translate-x-24;
     }
+
     &-icon {
       @apply text-xl text-primary fill-current transition duration-300;
+
       &:hover {
         @apply text-primary-alt;
       }
     }
   }
+
   &-img {
     @apply inline-flex justify-center;
+
     @screen md {
       @apply order-1 justify-self-center;
     }
   }
+
   &-blob {
     @apply w-48 fill-current text-primary;
+
     @screen lg {
       @apply w-72;
     }
+
     @screen xl {
       @apply w-80;
     }
+
     &-img {
       @apply w-44;
     }
   }
+
   &-data {
     grid-column: 1/3;
+
     @screen md {
       grid-column: initial;
     }
   }
+
   &-title {
     @apply text-2xl;
   }
+
   &-subtitle {
     @apply text-xl font-medium mb-3 text-primary-text;
   }
+
   &-description {
     @apply mb-8;
   }
+
   &-scroll {
     @screen lg {
       @apply block;
     }
+
     &-button {
       @screen lg {
         @apply ml-12;
       }
+
       @apply flex items-center text-primary transition duration-300;
+
       &:hover {
         @apply transform translate-y-1;
       }
     }
+
     &-name {
       @apply text-sm text-primary-title font-medium mr-1;
     }
