9b72ed7f6d5e9be5c287497fec0fa494d70a26a0
bertyn99 <bboulikou@gmail.com>
2024-10-22 16:40:02 +0200

    fix change cta

--- components/Home.vue
+++ components/Home.vue
@@ -49,8 +49,9 @@
             Freelance Developpeur fullstack, produisant du travail de qualité et
             toujours curieux de découvrir de nouvelles technologies.
           </p>
-          <the-button link="#contact" class="button button-flex">Contactez-moi<svg xmlns="http://www.w3.org/2000/svg"
-              class="button-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <the-button link="https://cal.com/bertyn-boulikou" class="button button-flex">Discutons de votre projet<svg
+              xmlns="http://www.w3.org/2000/svg" class="button-icon" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             </svg></the-button>
