ec6c595097cc372dcb43bf130fff1da0839467f0
bertyn99 <bboulikou@gmail.com>
2026-05-11 14:11:15 +0200

    fix: make download button work by rendering <a> tag instead of nuxt-link

--- components/TheButton.vue
+++ components/TheButton.vue
@@ -1,13 +1,13 @@
 <template>
-  <nuxt-link :to="link"><slot></slot> </nuxt-link>
-</template>
+  <a v-if="download" :href="link" :download="download"><slot></slot></a>
+  <nuxt-link v-else :to="link"><slot></slot></nuxt-link>
 
 <script>
 export default {
-  props: {
-    link: { type: String, require: true, default: '' },
-  },
-}
+    props: {
+      link: { type: String, require: true, default: '' },
+      download: { type: String, default: null },
+    },
 </script>
 
 <style>
